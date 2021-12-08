3ee95609f6730876e98a2f9aa14fe3d3ef6e6728
Kamil Gafurov <gafurovkg@yandex.ru>
2021-12-08 16:43:17 +0300

    0

---
+++ M01_Introduction/M01_Introduction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M01_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
