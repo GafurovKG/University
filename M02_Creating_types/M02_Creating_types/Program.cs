64e91a10a4cc6328c1d4e4cad9a829243f44a556
Kamil Gafurov <gafurovkg@yandex.ru>
2021-12-13 00:28:37 +0300

    задание 1 готово

--- M02_Creating_types/M02_Creating_types/Program.cs
+++ M02_Creating_types/M02_Creating_types/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace M02_Creating_types
 {
@@ -6,7 +7,43 @@ namespace M02_Creating_types
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] subjects = { "Maths", "Philosophy", "History", "PE", "English", "Sport" };
+
+            var student1c1 = new Student("pete.pupkin@epam.com");
+            var student2c1 = new Student("vasa.vasechkin@epam.com");
+            var student3c1 = new Student("kola.kolishev@epam.com");
+
+            var student1c2 = new Student("pete", "pupkin");
+            var student2c2 = new Student("vasa", "vasechkin");
+            var student3c2 = new Student("kola", "kolishev");
+
+            var students = new List<Student>() { student1c1, student1c2, student2c1, student2c2, student3c1, student3c2 };
+
+            Dictionary<Student, HashSet<string>> studentSubjectDict = new();
+
+            Random rand = new Random();
+
+            //запоняем studentSubjectDict через циклы
+            foreach (var stud in students)
+            {
+                HashSet<string> subj = new HashSet<string>();
+                do
+                {
+                    subj.Add(subjects[rand.Next(5)]); //предметы тожене должны повтояться
+                } while (subj.Count<=2);
+                studentSubjectDict.TryAdd(stud, subj);
+            }
+
+            //проверка studentSubjectDict
+            foreach (var dict in studentSubjectDict)
+            {
+                Console.WriteLine(dict.Key.ShowInfo());
+                foreach (var subj in dict.Value)
+                {
+                    Console.Write(subj + " ");
+                }
+                Console.WriteLine("\n");
+            }
         }
     }
 }
---
+++ M02_Creating_types/M02_Creating_types/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02_Creating_types
+{
+    internal class Student
+    {
+        private string fullName;
+        private string email;
+
+        public Student (string email)
+        {
+            this.email = email;
+            string[] emailsplit = email.Split('.', '@');
+            this.fullName = emailsplit[0] + " " + emailsplit[1]; // поскать дргуой способ
+            
+        }
+        public Student (string name, string surname)
+        {
+            this.fullName = name + " " + surname;
+            this.email = name + "." + surname + "@epam.com";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                   email == student.email;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(email, fullName);
+        }
+        public string ShowInfo()
+        {
+            return this.fullName + " " + this.email + ":";
+        }
+    }
+}
