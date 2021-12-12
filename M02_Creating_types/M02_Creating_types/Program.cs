5d286ea08719691af423ecef47c9a23af4cc0ec2
Kamil Gafurov <gafurovkg@yandex.ru>
2021-12-12 16:19:40 +0300

    0

---
+++ M02_Creating_types/M02_Creating_types/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace M02_Creating_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
