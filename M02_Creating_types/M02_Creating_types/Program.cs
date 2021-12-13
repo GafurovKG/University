d4411a40d6967ca424671d4a742e870da29465a8
Kamil Gafurov <gafurovkg@yandex.ru>
2021-12-13 21:40:06 +0300

    Задача 1 собрана

--- M02_Creating_types/M02_Creating_types/Program.cs
+++ M02_Creating_types/M02_Creating_types/Program.cs
@@ -21,11 +21,7 @@ namespace M02_Creating_types
 
             Dictionary<Student, HashSet<string>> studentSubjectDict = new();
 
-            ////Если инициализировать Value при добавлении в Словарь, Value перезапишется
-            //studentSubjectDict[student1c1] = new HashSet<string> { "Maths", "Philosophy", "History" };
-            //studentSubjectDict[student1c2] = new HashSet<string> { "History", "PE", "English" };
-
-            //запоняем studentSubjectDict через циклы -Value сохраняется от первого варианта. 
+            //запоняем studentSubjectDict через циклы. При встече дубликатов по key хэш, они отлетают, Value остается от первого записаннго student 
             Random rand = new Random();
             foreach (var stud in students)
             {
@@ -35,15 +31,11 @@ namespace M02_Creating_types
                     subj.Add(subjects[rand.Next(5)]); //предметы тожене не должны повтояться
                 } while (subj.Count <= 2);
                 studentSubjectDict.TryAdd(stud, subj);
-
-                //// проверка какое из Value сохранилось
-                //Console.WriteLine(stud.ShowInfo());
-                //foreach (var subject in subj)
-                //{
-                //    Console.Write(subject + " ");
-                //}
-                //Console.WriteLine("\n");
             }
+
+            ////Если инициализировать Value при инициаизации элемента Dictionary, то Value перезапишется в имеющийся элемент с таким же хэш key
+            //studentSubjectDict[student1c1] = new HashSet<string> { "Maths", "Philosophy", "History" };
+            //studentSubjectDict[student1c2] = new HashSet<string> { "History", "PE", "English" };
 
             //проверка итогового studentSubjectDict
             foreach (var dict in studentSubjectDict)
