7240834bf91fccedb1c35f8c9597acd4f87f8be3
Kamil Gafurov <gafurovkg@yandex.ru>
2021-12-20 10:55:11 +0300

    Добавил ДЗ 3

---
+++ M03_Strings/M03_Strings/PhoneNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M03_Strings
+{
+    internal class PhoneNumbers
+    {
+        public static List<string> DoIt(string str)
+        {
+            var PhoneNumbers = new List<string>();
+            var PhoneNumberBuild = new StringBuilder();
+            for (int i =0; i < str.Length; i++)
+            {
+                if (Char.IsDigit(str[i]) || str[i].Equals( '(' ) || str[i].Equals( ')' ) || str[i].Equals( '-' ) || str[i].Equals( '+' ) || str[i].Equals(' '))
+                {
+                    PhoneNumberBuild.Append(str[i]);
+                }
+                else
+                {
+                    if (PhoneNumberBuild.Length > 10)
+                    {
+                        PhoneNumbers.Add(PhoneNumberBuild.ToString().Trim());
+                        PhoneNumberBuild.Clear();
+                    }
+                    else PhoneNumberBuild.Clear();
+                }
+            }
+            return PhoneNumbers;
+        }
+    }
+}
---
+++ M03_Strings/M03_Strings/ReverseWords.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M03_Strings
+{
+    internal static class ReverseWords
+    {
+        public static string DoIt (string str)
+        {
+            var words = str.Split(' ');
+            var StrBuilder = new StringBuilder();
+            for (int i = words.Length - 1; i >= 0 ; i--)
+            {
+                StrBuilder.Append(words[i]);
+                StrBuilder.Append(' ');
+            }
+            return StrBuilder.ToString ().TrimEnd();
+        }
+    }
+}
