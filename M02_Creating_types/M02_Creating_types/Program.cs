003e42a97e0293dc6cb48e86a59772685c841bd5
GafurovKG <GafurovKG@yandex.ru>
2021-12-25 18:30:42 +0300

    Исправления CodeStyle

--- M02_Creating_types/M02_Creating_types/Program.cs
+++ M02_Creating_types/M02_Creating_types/Program.cs
@@ -5,10 +5,10 @@ namespace M02_Creating_types
 {
     internal class Program
     {
-        static void Main()
+        private static void Main()
         {
             string[] subjects = { "Maths", "Philosophy", "History", "PE", "English", "Sport" };
-            
+
             var student1c1 = new Student("pete.pupkin@epam.com");
             var student2c1 = new Student("vasa.vasechkin@epam.com");
             var student3c1 = new Student("kola.kolishev@epam.com");
@@ -21,23 +21,24 @@ namespace M02_Creating_types
 
             Dictionary<Student, HashSet<string>> studentSubjectDict = new();
 
-            //запоняем studentSubjectDict через циклы. При встече дубликатов по key хэш, они отлетают, Value остается от первого записаннго student 
-            Random rand = new ();
+            // запоняем studentSubjectDict через циклы. При встече дубликатов по key хэш, они отлетают, Value остается от первого записаннго student
+            Random rand = new();
             foreach (var stud in students)
             {
-                HashSet<string> subj = new ();
+                HashSet<string> subj = new();
                 do
                 {
-                    subj.Add(subjects[rand.Next(5)]); //предметы тожене не должны повтояться
-                } while (subj.Count <= 2);
+                    subj.Add(subjects[rand.Next(5)]);
+                }
+                while (subj.Count <= 2);
                 studentSubjectDict.TryAdd(stud, subj);
             }
 
-            ////Если инициализировать Value при инициаизации элемента Dictionary, то Value перезапишется в имеющийся элемент с таким же хэш key
-            //studentSubjectDict[student1c1] = new HashSet<string> { "Maths", "Philosophy", "History" };
-            //studentSubjectDict[student1c2] = new HashSet<string> { "History", "PE", "English" };
+            // Если инициализировать Value при инициаизации элемента Dictionary, то Value перезапишется в имеющийся элемент с таким же хэш key
+            // studentSubjectDict[student1c1] = new HashSet<string> { "Maths", "Philosophy", "History" };
+            // studentSubjectDict[student1c2] = new HashSet<string> { "History", "PE", "English" };
 
-            //проверка итогового studentSubjectDict
+            // проверка итогового studentSubjectDict
             foreach (var dict in studentSubjectDict)
             {
                 Console.WriteLine(dict.Key.ShowInfo());
@@ -45,6 +46,7 @@ namespace M02_Creating_types
                 {
                     Console.Write(subj + " ");
                 }
+
                 Console.WriteLine("\n");
             }
         }
