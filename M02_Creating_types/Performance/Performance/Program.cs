a84cb0c31a27b4e09abcaa103b5e4aef37aa55c6
GafurovKG <GafurovKG@yandex.ru>
2021-12-27 10:07:35 +0300

    CodeStyle испоавления в Performance

--- M02_Creating_types/Performance/Performance/Program.cs
+++ M02_Creating_types/Performance/Performance/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
-using System.Collections.Generic;
-
 
 namespace Performance
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             Process currentProcess = Process.GetCurrentProcess();
             var rdm = new Random();
@@ -17,21 +14,23 @@ namespace Performance
             long mem0 = currentProcess.PrivateMemorySize64;
             C[] classes = new C[n];
             long memCreateClassArray = currentProcess.PrivateMemorySize64 - mem0;
-            
+
             for (int i = 0; i < n; i++)
             {
-                classes[i] = new C { i = rdm.Next(n) };
+                classes[i] = new C { I = rdm.Next(n) };
             }
+
             long memInitClassArray = currentProcess.PrivateMemorySize64 - memCreateClassArray;
-            
+
             S[] structs = new S[n];
             long memCreatStructArray = currentProcess.PrivateMemorySize64 - memInitClassArray;
 
             for (int i = 0; i < n; i++)
             {
-                structs[i].i = rdm.Next(n);
+                structs[i].I = rdm.Next(n);
             }
-            long memInitStructArray =currentProcess.PrivateMemorySize64 - memCreatStructArray;
+
+            long memInitStructArray = currentProcess.PrivateMemorySize64 - memCreatStructArray;
 
             Console.WriteLine("memCreateClassArray: " + memCreateClassArray);
             Console.WriteLine("memInitClassArray:" + memInitClassArray);
@@ -39,39 +38,42 @@ namespace Performance
             Console.WriteLine("memInitStructArray: " + memInitStructArray);
 
             var stopWatch = new Stopwatch();
-            //время сортировки массива класса
+
+            // время сортировки массива класса
             stopWatch.Start();
-            Array.Sort<C>(classes);
+            Array.Sort(classes);
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
             Console.WriteLine("RunTime ClassSort" + elapsedTime);
             stopWatch.Restart();
 
-            //время сортировки массива структуры
+            // время сортировки массива структуры
             stopWatch.Start();
-            Array.Sort<S>(structs);
+            Array.Sort(structs);
             stopWatch.Stop();
             ts = stopWatch.Elapsed;
-            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
             Console.WriteLine("RunTime Structort" + elapsedTime);
         }
-        public class C: IComparable<C>
+
+        public class C : IComparable<C>
         {
-            public int i;
+            public int I { get; set; }
 
             int IComparable<C>.CompareTo(C other)
             {
-                return this.i.CompareTo(other.i);
+                return this.I.CompareTo(other.I);
             }
         }
+
         public struct S : IComparable<S>
         {
-            public int i;
+            public int I { get; set; }
 
             int IComparable<S>.CompareTo(S other)
             {
-                return this.i.CompareTo(other.i);
+                return this.I.CompareTo(other.I);
             }
         }
     }
