669397d79908ec1d25919379b9dca7aaa1c4b4d8
GafurovKG <GafurovKG@yandex.ru>
2022-01-11 21:57:09 +0300

    BinarySearch + test

---
+++ M08_Generics_Collections/MyGenetics/MyBinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace MyGenetics
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class MyBinarySearch<T>
+        where T : IComparable<T>
+    {
+        public static int BinarySearch<T1>(IEnumerable<T1> values, T1 searchable)
+            where T1 : IComparable<T1>
+        {
+            int center = values.Count() / 2;
+            int border = values.Count();
+            do
+            {
+                if (values.ElementAt(center).CompareTo(searchable) > 0)
+                {
+                    border = center;
+                    center /= 2;
+                }
+                else if (values.ElementAt(center).CompareTo(searchable) < 0)
+                {
+                    center += (border - center) / 2;
+                }
+            }
+            while (values.ElementAt(center).CompareTo(searchable) != 0);
+
+            return center;
+        }
+    }
+}
